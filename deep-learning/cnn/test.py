07e339c85f00de0bc2b4181294dd90df5870c786
asiclzscl@gmail.com <asiclzscl@gmail.com>
2017-02-24 16:20:04 -0800

    init

---
+++ deep-learning/cnn/test.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+# Create a Variable, that will be initialized to the scalar value 0.
+state = tf.Variable(0, name="counter")
+
+# Create an Op to add one to `state`.
+
+session = tf.InteractiveSession()
+
+one = tf.constant(1)
+new_value = tf.add(state, one)
+update = tf.assign(state, new_value)
+
+# Variables must be initialized by running an `init` Op after having
+# launched the graph.  We first have to add the `init` Op to the graph.
+init_op = tf.global_variables_initializer()
+session.run(init_op)
+print (update.eval())
+
+session.close()
+
+## Launch the graph and run the ops.
+#with tf.Session() as sess:
+#  # Run the 'init' op
+#  sess.run(init_op)
+#  # Print the initial value of 'state'
+#  print(sess.run(state))
+#  # Run the op that updates 'state' and print 'state'.
+#  for _ in range(3):
+#    sess.run(update)
+#    print(sess.run(state))
---
+++ deep-learning/cnn/test2.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+# Create a variable.
+w = tf.Variable(<initial-value>, name=<optional-name>)
+
+# Use the variable in the graph like any Tensor.
+y = tf.matmul(w, ...another variable or tensor...)
+
+# The overloaded operators are available too.
+z = tf.sigmoid(w + y)
+
+# Assign a new value to the variable with `assign()` or a related method.
+w.assign(w + 1.0)
+w.assign_add(1.0)
